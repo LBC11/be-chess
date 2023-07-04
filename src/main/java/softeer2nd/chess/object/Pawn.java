7adeb4b511e09fd178fd9ef0eda5eef97d269164
이병찬 <lbc@ibyeongchan-ui-noteubug.local>
2023-07-04 17:30:17 +0900

    feat: Pawn class 구현 - color는 생성자에서 결정 - color 확인을 위한 getColor() 추가

---
+++ src/main/java/softeer2nd/chess/object/Pawn.java
@@ -0,0 +1,14 @@
+package softeer2nd.chess.object;
+
+public class Pawn {
+
+    String color;
+
+    public Pawn(String color) {
+        this.color = color;
+    }
+
+    public String getColor() {
+        return color;
+    }
+}

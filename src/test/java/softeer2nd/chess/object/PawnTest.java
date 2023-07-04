09c9efede3bdb629135f5bc169e133d21e3e063f
이병찬 <lbc@ibyeongchan-ui-noteubug.local>
2023-07-04 17:35:12 +0900

    test: 테스트 코드 추가
    - 기존 create 검증
    - verifyPawn()으로 중복 제

---
+++ src/test/java/softeer2nd/chess/object/PawnTest.java
@@ -0,0 +1,33 @@
+package softeer2nd.chess.object;
+
+import org.junit.jupiter.api.*;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class PawnTest {
+
+    @Test
+    @DisplayName("폰이 정상적으로 생성되어야 한다")
+    public void create() {
+
+        String color = "white";
+        Pawn pawn = new Pawn(color);
+        assertThat(pawn.getColor()).isEqualTo(color);
+
+        color = "black";
+        pawn = new Pawn(color);
+        assertThat(pawn.getColor()).isEqualTo("black");
+    }
+
+    @Test
+    @DisplayName("중복 제거")
+    public void create2() {
+        assertThat(verifyPawn("white")).isEqualTo(true);
+        assertThat(verifyPawn("black")).isEqualTo(true);
+    }
+
+    private boolean verifyPawn(final String color) {
+        Pawn pawn = new Pawn(color);
+        return pawn.getColor().equals(color);
+    }
+}

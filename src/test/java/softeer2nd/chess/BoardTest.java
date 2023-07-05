828507c6008e401d084aab833b51192ee7fa3747
이병찬 <lbc@ibyeongchan-ui-noteubug.local>
2023-07-05 13:19:25 +0900

    refactor: BoardTest 중복 제거
    - BeforeEach를 사용
    - verifyBoard(Pawn pawn): board의 method들이 정상작동하는지 검증

--- src/test/java/softeer2nd/chess/BoardTest.java
+++ src/test/java/softeer2nd/chess/BoardTest.java
@@ -3,22 +3,35 @@ package softeer2nd.chess;
 import static org.junit.Assert.*;
 
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import softeer2nd.chess.pieces.Pawn;
 
 public class BoardTest {
+    Board board;
+    Pawn white;
+    Pawn black;
+    int size;
+
+    @BeforeEach
+    public void init() {
+        board = new Board();
+        white = new Pawn(Pawn.WHITE_COLOR);
+        black = new Pawn(Pawn.BLACK_COLOR);
+        size = 0;
+    }
     @Test
+    @DisplayName("board 가 정상적으로 생성되어야 한다.")
     public void create() throws Exception {
-        Board board = new Board();
 
-        Pawn white = new Pawn(Pawn.WHITE_COLOR);
-        board.add(white);
-        assertEquals(1, board.size());
-        assertEquals(white, board.findPawn(0));
+        verifyBoard(white);
+        verifyBoard(black);
+    }
 
-        Pawn black = new Pawn(Pawn.BLACK_COLOR);
-        board.add(black);
-        assertEquals(2, board.size());
-        assertEquals(black, board.findPawn(1));
+    private void verifyBoard(Pawn pawn) {
+        board.add(pawn);
+        assertEquals(++size, board.size());
+        assertEquals(pawn, board.findPawn(size - 1));
     }
 }

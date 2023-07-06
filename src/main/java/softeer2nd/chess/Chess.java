21939784ab761897f90bae19222e0b99fc6f7cd8
이병찬 <lbc@ibyeongchan-ui-noteubug.local>
2023-07-06 15:26:36 +0900

    feat: CHESS CLASS 구현
    - boolean isStarted를 활용하여 게임중인지 확인
    - 각 method는 상황에 맞는 메시지를 반환

--- src/main/java/softeer2nd/chess/Chess.java
+++ src/main/java/softeer2nd/chess/Chess.java
@@ -1,6 +1,30 @@
-package softeer2nd.chess.pieces;
+package softeer2nd.chess;
 
 public class Chess {
 
+    private Board board;
+    private boolean isStarted;
 
+    public void init() {
+        board = new Board();
+        isStarted = false;
+    }
+
+    public String start() {
+
+        if(isStarted) return "이미 게임중입니다.";
+
+        board.initialize();
+        board.print();
+        isStarted = true;
+        return "게임이 시작되었습니다.";
+    }
+
+    public String end() {
+
+        if(!isStarted) return "게임를 하고 있지 않습니다.";
+
+        isStarted = false;
+        return "게임이 종료되었습니다.";
+    }
 }

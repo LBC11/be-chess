85c273945892096a34e86899f5a1f53bf3525c09
이병찬 <lbc@ibyeongchan-ui-noteubug.local>
2023-07-06 12:00:23 +0900

    test: PAWN 테스트 추가
    기존 테스트 삭제

--- src/test/java/softeer2nd/chess/pieces/PawnTest.java
+++ src/test/java/softeer2nd/chess/pieces/PawnTest.java
@@ -2,40 +2,29 @@ package softeer2nd.chess.pieces;
 
 import org.junit.jupiter.api.*;
 
-import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class PawnTest {
 
     @Test
-    @DisplayName("폰이 정상적으로 생성되어야 한다")
-    public void create() {
-
-        String color = "white";
-        Pawn pawn = new Pawn(color);
-        assertThat(pawn.getColor()).isEqualTo(color);
-
-        color = "black";
-        pawn = new Pawn(color);
-        assertThat(pawn.getColor()).isEqualTo("black");
-    }
-
-    @Test
-    @DisplayName("중복 제거")
-    public void create2() {
-        assertThat(verifyPawn("white")).isEqualTo(true);
-        assertThat(verifyPawn("black")).isEqualTo(true);
-    }
-
-    private boolean verifyPawn(final String color) {
-        Pawn pawn = new Pawn(color);
-        return pawn.getColor().equals(color);
-    }
-
-    @Test
+    @DisplayName("Pawn 에 변수를 주지 않아도 정상적으로 하얀색 pawn 이 생성되어야 합니다. ")
     public void create_기본생성자() throws Exception {
         Pawn pawn = new Pawn();
-        assertEquals("white", pawn.getColor());
+        assertEquals(Pawn.WHITE_COLOR, pawn.getColor());
+        assertEquals(Pawn.WHITE_REPRESENTATION, pawn.getRepresentation());
+    }
+
+    @Test
+    @DisplayName("폰이 정상적으로 생성되어야 한다")
+    public void create() {
+        verifyPawn(Pawn.WHITE_COLOR, Pawn.WHITE_REPRESENTATION);
+        verifyPawn(Pawn.BLACK_COLOR, Pawn.BLACK_REPRESENTATION);
+    }
+
+    void verifyPawn(final String color, final char representation) {
+        Pawn pawn = new Pawn(color, representation);
+        assertEquals(color, pawn.getColor());
+        assertEquals(representation, pawn.getRepresentation());
     }
 
 }

ef5b6c37307d79ffb5fb13bd2363ad7919c835c1
이병찬 <lbc@ibyeongchan-ui-noteubug.local>
2023-07-06 12:03:46 +0900

    feat: PAWN CLASS 추가 구현
    - field에 representation 추가
    - getRepresentation method 추가
    - 중복을 줄이기 위해 각 색깔의 representation 상수로 구현

--- src/main/java/softeer2nd/chess/pieces/Pawn.java
+++ src/main/java/softeer2nd/chess/pieces/Pawn.java
@@ -4,17 +4,28 @@ public class Pawn {
 
     public static final String WHITE_COLOR = "white";
     public static final String BLACK_COLOR = "black";
+    public static final String EMPTY_COLOR = "empty";
+    public static final char WHITE_REPRESENTATION = 'p';
+    public static final char BLACK_REPRESENTATION = 'P';
+    public static final char EMPTY_REPRESENTATION = 'e';
 
     String color = WHITE_COLOR;
+    char representation = WHITE_REPRESENTATION;
 
     public Pawn() {
     }
 
-    public Pawn(String color) {
+    public Pawn(String color, char representation) {
+
         this.color = color;
+        this.representation = representation;
     }
 
     public String getColor() {
         return color;
+    }
+
+    public char getRepresentation() {
+        return this.representation;
     }
 }

13bd3092ed9f52f85ba1e310f58b3127b6311fb8
이병찬 <lbc@ibyeongchan-ui-noteubug.local>
2023-07-06 12:06:00 +0900

    test: BOARD 테스트 추가
    - 기존 테스트는 삭제

--- src/test/java/softeer2nd/chess/BoardTest.java
+++ src/test/java/softeer2nd/chess/BoardTest.java
@@ -3,35 +3,16 @@ package softeer2nd.chess;
 import static org.junit.Assert.*;
 
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import softeer2nd.chess.pieces.Pawn;
 
 public class BoardTest {
-    Board board;
-    Pawn white;
-    Pawn black;
-    int size;
-
-    @BeforeEach
-    public void init() {
-        board = new Board();
-        white = new Pawn(Pawn.WHITE_COLOR);
-        black = new Pawn(Pawn.BLACK_COLOR);
-        size = 0;
-    }
     @Test
     @DisplayName("board 가 정상적으로 생성되어야 한다.")
-    public void create() throws Exception {
-
-        verifyBoard(white);
-        verifyBoard(black);
-    }
-
-    private void verifyBoard(Pawn pawn) {
-        board.add(pawn);
-        assertEquals(++size, board.size());
-        assertEquals(pawn, board.findPawn(size - 1));
+    public void initialize() throws Exception {
+        Board board = new Board();
+        board.initialize();
+        assertEquals("pppppppp", board.getWhitePawnsResult());
+        assertEquals("PPPPPPPP", board.getBlackPawnsResult());
     }
 }

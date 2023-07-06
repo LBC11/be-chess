7162e2c8e30bd3967d355808a34e22a830cd4445
이병찬 <lbc@ibyeongchan-ui-noteubug.local>
2023-07-06 15:44:41 +0900

    feat: MAIN CLASS 구현

--- src/main/java/softeer2nd/Main.java
+++ src/main/java/softeer2nd/Main.java
@@ -1,8 +1,36 @@
 package softeer2nd;
 
+import softeer2nd.chess.Chess;
+
+import java.util.Scanner;
+
 public class Main {
+
+    static Chess chess;
+
     public static void main(String[] args) {
 
-        System.out.println();
+        Scanner sc = new Scanner(System.in);
+
+        chess = new Chess();
+        chess.init();
+
+        // 2중 반복문을 한번에 종료하기 위한 라벨
+        outer:
+        while(true) {
+
+            System.out.print("명령어를 입력해주세요: ");
+            String cmd = sc.nextLine();
+
+            switch (cmd) {
+                case "start" -> System.out.println(chess.start());
+                case "end" -> System.out.println(chess.end());
+                case "exit" -> {
+                    System.out.println("시스템을 종료합니다.");
+                    break outer;
+                }
+                default -> System.out.println("올바른 명령어가 아닙니다.");
+            }
+        }
     }
 }

aa9ca886c86e219f223c75ff03b7a7ef0121aabe
이병찬 <lbc@ibyeongchan-ui-noteubug.local>
2023-07-06 18:41:57 +0900

    test: STRINGUTILS 테스트 추가

---
+++ src/test/java/softeer2nd/utils/StringUtilsTest.java
@@ -0,0 +1,23 @@
+package softeer2nd.utils;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StringUtilsTest {
+
+    @Test
+    @DisplayName("개행문자가 정상적으로 추가된다.")
+    public void appendNewLine() {
+
+        String a = "a";
+        String b = "b";
+        assertEquals(a + "\n" + b, StringUtils.appendNewLine(a) + b);
+
+        StringBuilder sb1 = new StringBuilder();
+        StringBuilder sb2 = new StringBuilder();
+        assertEquals(sb2.append(a).append("\n").append(b).toString(), StringUtils.appendNewLine(sb1.append(a)).append(b).toString());
+    }
+
+}

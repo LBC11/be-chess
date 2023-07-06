bfe60ad0e571e90062afd77d3c9c1dcd18e68f58
이병찬 <lbc@ibyeongchan-ui-noteubug.local>
2023-07-06 18:42:29 +0900

    feat: STRINGUTILS 구현

--- src/main/java/softeer2nd/utils/StringUtils.java
+++ src/main/java/softeer2nd/utils/StringUtils.java
@@ -1,4 +1,16 @@
-package softeer2nd.chess.utils;
+package softeer2nd.utils;
 
 public class StringUtils {
+
+    public static final String NEWLINE = System.getProperty("line.separator");
+
+    private StringUtils() {}
+
+    public static String appendNewLine(String s) {
+        return s + NEWLINE;
+    }
+
+    public static StringBuilder appendNewLine(StringBuilder sb) {
+        return sb.append(NEWLINE);
+    }
 }

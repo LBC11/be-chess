a0bb6ec98fc5bb40f716a3694af4df2f3d39ef34
이병찬 <lbc@ibyeongchan-ui-noteubug.local>
2023-07-07 12:07:32 +0900

    refactor: PIECE CLASS에서 팩토리 메소드에서의 중복 제거

--- src/main/java/softeer2nd/chess/pieces/Piece.java
+++ src/main/java/softeer2nd/chess/pieces/Piece.java
@@ -51,51 +51,8 @@ public class Piece {
         return this.color == Color.BLACK ? StringUtils.Uppercase(representation) : representation;
     }
 
-    public static Piece createWhitePawn() {
-        return new Piece(Type.PAWN, Color.WHITE);
-    }
-    public static Piece createBlackPawn() {
-        return new Piece(Type.PAWN, Color.BLACK);
-    }
-
-    public static Piece createWhiteKnight() {
-        return new Piece(Type.KNIGHT, Color.WHITE);
-    }
-
-    public static Piece createBlackKnight() {
-        return new Piece(Type.KNIGHT, Color.BLACK);
-    }
-
-    public static Piece createWhiteRook() {
-        return new Piece(Type.ROOK, Color.WHITE);
-    }
-
-    public static Piece createBlackRook() {
-        return new Piece(Type.ROOK, Color.BLACK);
-    }
-
-    public static Piece createWhiteBishop() {
-        return new Piece(Type.BISHOP, Color.WHITE);
-    }
-
-    public static Piece createBlackBishop() {
-        return new Piece(Type.BISHOP, Color.BLACK);
-    }
-
-    public static Piece createWhiteQueen() {
-        return new Piece(Type.QUEEN, Color.WHITE);
-    }
-
-    public static Piece createBlackQueen() {
-        return new Piece(Type.QUEEN, Color.BLACK);
-    }
-
-    public static Piece createWhiteKing() {
-        return new Piece(Type.KING, Color.WHITE);
-    }
-
-    public static Piece createBlackKing() {
-        return new Piece(Type.KING, Color.BLACK);
+    public static Piece create(Type type, Color color) {
+        return new Piece(type, color);
     }
 
 }

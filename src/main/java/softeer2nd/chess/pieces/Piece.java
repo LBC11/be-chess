fef337e6cac34bfe60bc69f0d740deb283dcf97a
이병찬 <lbc@ibyeongchan-ui-noteubug.local>
2023-07-07 11:16:44 +0900

    feat: PIECE 구현
    - 중복을 줄이기 위해 enum을 사용해 type, color 구현
    - 기존의 생성 방식을 제거하고 팩토리 메소드 도입

--- src/main/java/softeer2nd/chess/pieces/Piece.java
+++ src/main/java/softeer2nd/chess/pieces/Piece.java
@@ -1,31 +1,101 @@
 package softeer2nd.chess.pieces;
 
+import softeer2nd.utils.StringUtils;
+
 public class Piece {
 
-    public static final String WHITE_COLOR = "white";
-    public static final String BLACK_COLOR = "black";
-    public static final String EMPTY_COLOR = "empty";
-    public static final char WHITE_REPRESENTATION = 'p';
-    public static final char BLACK_REPRESENTATION = 'P';
-    public static final char EMPTY_REPRESENTATION = '.';
-
-    private String color = WHITE_COLOR;
-    private char representation = WHITE_REPRESENTATION;
-
-    public Piece() {
+    public enum Color {
+        WHITE, BLACK, EMPTY
     }
 
-    public Piece(String color, char representation) {
 
+    public enum Type {
+
+        PAWN('p'), KNIGHT('n'), ROOK('r'),
+        BISHOP('b'), QUEEN('q'), KING('k'),
+        EMPTY('.');
+
+        private final char representation;
+
+        Type(char representation) {
+
+            this.representation = representation;
+        }
+
+        public char getRepresentation() {
+            return this.representation;
+        }
+    }
+
+    private final Type type;
+    private final Color color;
+
+    private Piece(Type type, Color color) {
+
+        this.type = type;
         this.color = color;
-        this.representation = representation;
     }
 
-    public String getColor() {
+    public Color getColor() {
         return color;
     }
 
-    public char getRepresentation() {
-        return this.representation;
+    public Type getType() {
+        return this.type;
     }
+
+    public char getRepresentation() {
+
+        char representation = this.type.getRepresentation();
+
+        return this.color == Color.BLACK ? StringUtils.Uppercase(representation) : representation;
+    }
+
+    public static Piece createWhitePawn() {
+        return new Piece(Type.PAWN, Color.WHITE);
+    }
+    public static Piece createBlackPawn() {
+        return new Piece(Type.PAWN, Color.BLACK);
+    }
+
+    public static Piece createWhiteKnight() {
+        return new Piece(Type.KNIGHT, Color.WHITE);
+    }
+
+    public static Piece createBlackKnight() {
+        return new Piece(Type.KNIGHT, Color.BLACK);
+    }
+
+    public static Piece createWhiteRook() {
+        return new Piece(Type.ROOK, Color.WHITE);
+    }
+
+    public static Piece createBlackRook() {
+        return new Piece(Type.ROOK, Color.BLACK);
+    }
+
+    public static Piece createWhiteBishop() {
+        return new Piece(Type.BISHOP, Color.WHITE);
+    }
+
+    public static Piece createBlackBishop() {
+        return new Piece(Type.BISHOP, Color.BLACK);
+    }
+
+    public static Piece createWhiteQueen() {
+        return new Piece(Type.QUEEN, Color.WHITE);
+    }
+
+    public static Piece createBlackQueen() {
+        return new Piece(Type.QUEEN, Color.BLACK);
+    }
+
+    public static Piece createWhiteKing() {
+        return new Piece(Type.KING, Color.WHITE);
+    }
+
+    public static Piece createBlackKing() {
+        return new Piece(Type.KING, Color.BLACK);
+    }
+
 }

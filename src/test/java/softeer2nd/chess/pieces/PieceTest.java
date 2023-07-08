922925079ab40a8f166f25c46a31133227d66257
이병찬 <lbc@ibyeongchan-ui-noteubug.local>
2023-07-08 21:49:06 +0900

    test: PIECE TYPE 검증을 위한 테스트 수정
    - isBlack()과 같은 메소드를 타입에도 적용하기 위해 테스트 수정

--- src/test/java/softeer2nd/chess/pieces/PieceTest.java
+++ src/test/java/softeer2nd/chess/pieces/PieceTest.java
@@ -4,41 +4,58 @@ import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class PieceTest {
-
-    @Test
-    @DisplayName("기물이 정상적으로 생성되어야 한다")
-    public void create_piece() {
-        verifyPiece(Piece.create(Piece.Type.PAWN, Piece.Color.WHITE), Piece.Color.WHITE, Piece.Type.PAWN);
-        verifyPiece(Piece.create(Piece.Type.PAWN, Piece.Color.BLACK), Piece.Color.BLACK, Piece.Type.PAWN);
-        verifyPiece(Piece.create(Piece.Type.KNIGHT, Piece.Color.WHITE), Piece.Color.WHITE, Piece.Type.KNIGHT);
-        verifyPiece(Piece.create(Piece.Type.KNIGHT, Piece.Color.BLACK), Piece.Color.BLACK, Piece.Type.KNIGHT);
-        verifyPiece(Piece.create(Piece.Type.ROOK, Piece.Color.WHITE), Piece.Color.WHITE, Piece.Type.ROOK);
-        verifyPiece(Piece.create(Piece.Type.ROOK, Piece.Color.BLACK), Piece.Color.BLACK, Piece.Type.ROOK);
-        verifyPiece(Piece.create(Piece.Type.BISHOP, Piece.Color.WHITE), Piece.Color.WHITE, Piece.Type.BISHOP);
-        verifyPiece(Piece.create(Piece.Type.BISHOP, Piece.Color.BLACK), Piece.Color.BLACK, Piece.Type.BISHOP);
-        verifyPiece(Piece.create(Piece.Type.QUEEN, Piece.Color.WHITE), Piece.Color.WHITE, Piece.Type.QUEEN);
-        verifyPiece(Piece.create(Piece.Type.QUEEN, Piece.Color.BLACK), Piece.Color.BLACK, Piece.Type.QUEEN);
-        verifyPiece(Piece.create(Piece.Type.KING, Piece.Color.WHITE), Piece.Color.WHITE, Piece.Type.KING);
-        verifyPiece(Piece.create(Piece.Type.KING, Piece.Color.BLACK), Piece.Color.BLACK, Piece.Type.KING);
-
-    }
-
-    private void verifyPiece(final Piece piece, final Piece.Color color, final Piece.Type type) {
-        assertEquals(type, piece.getType());
-        assertEquals(true, color == Piece.Color.WHITE ? piece.isWhite() : piece.isBlack());
-    }
+class PieceTest {
 
     @Test
     @DisplayName("기물의 색깔이 정상적으로 검증되어야 한다.")
-    public void colorCheck() {
+    public void verifyColor() {
 
         Piece whitePawn = Piece.create(Piece.Type.PAWN, Piece.Color.WHITE);
         Piece blackPawn = Piece.create(Piece.Type.PAWN, Piece.Color.BLACK);
 
-        assertEquals(true, whitePawn.isWhite());
-        assertEquals(false, whitePawn.isBlack());
-        assertEquals(true, blackPawn.isBlack());
-        assertEquals(false, blackPawn.isWhite());
+        verifyWhiteColor(whitePawn);
+        verifyBlackColor(blackPawn);
+    }
+
+    private void verifyWhiteColor(Piece piece) {
+        assertEquals(true, piece.isWhite());
+    }
+
+    private void verifyBlackColor(Piece piece) {
+        assertEquals(true, piece.isBlack());
+    }
+
+    @Test
+    @DisplayName("기물의 타입이 정상적으로 검증되어야 한다.")
+    public void verifyType() {
+        verifyPawnType(Piece.create(Piece.Type.PAWN, Piece.Color.WHITE));
+        verifyKnightType(Piece.create(Piece.Type.KNIGHT, Piece.Color.WHITE));
+        verifyRookType(Piece.create(Piece.Type.ROOK, Piece.Color.WHITE));
+        verifyBishopType(Piece.create(Piece.Type.BISHOP, Piece.Color.WHITE));
+        verifyQueenType(Piece.create(Piece.Type.QUEEN, Piece.Color.WHITE));
+        verifyKingType(Piece.create(Piece.Type.KING, Piece.Color.WHITE));
+    }
+
+    private void verifyPawnType(Piece piece) {
+        assertEquals(true, piece.isPawn());
+    }
+
+    private void verifyKnightType(Piece piece) {
+        assertEquals(true, piece.isKnight());
+    }
+
+    private void verifyRookType(Piece piece) {
+        assertEquals(true, piece.isRook());
+    }
+
+    private void verifyBishopType(Piece piece) {
+        assertEquals(true, piece.isBishop());
+    }
+
+    private void verifyQueenType(Piece piece) {
+        assertEquals(true, piece.isQueen());
+    }
+    private void verifyKingType(Piece piece) {
+        assertEquals(true, piece.isKing());
     }
 }

5b13a27f2d58e00670778d13d47a92794a1fd219
이병찬 <lbc@ibyeongchan-ui-noteubug.local>
2023-07-09 20:54:06 +0900

    test: COLOR, TYPE을 PIECE에서만 접근가능하게 하기 위한 테스트 수정

--- src/test/java/softeer2nd/chess/pieces/PieceTest.java
+++ src/test/java/softeer2nd/chess/pieces/PieceTest.java
@@ -10,8 +10,8 @@ class PieceTest {
     @DisplayName("기물의 색깔이 정상적으로 검증되어야 한다.")
     public void verifyColor() {
 
-        Piece whitePawn = Piece.create(Piece.Type.PAWN, Piece.Color.WHITE);
-        Piece blackPawn = Piece.create(Piece.Type.PAWN, Piece.Color.BLACK);
+        Piece whitePawn = Piece.createWhitePawn();
+        Piece blackPawn = Piece.createBlackPawn();
 
         verifyWhiteColor(whitePawn);
         verifyBlackColor(blackPawn);
@@ -28,12 +28,13 @@ class PieceTest {
     @Test
     @DisplayName("기물의 타입이 정상적으로 검증되어야 한다.")
     public void verifyType() {
-        verifyPawnType(Piece.create(Piece.Type.PAWN, Piece.Color.WHITE));
-        verifyKnightType(Piece.create(Piece.Type.KNIGHT, Piece.Color.WHITE));
-        verifyRookType(Piece.create(Piece.Type.ROOK, Piece.Color.WHITE));
-        verifyBishopType(Piece.create(Piece.Type.BISHOP, Piece.Color.WHITE));
-        verifyQueenType(Piece.create(Piece.Type.QUEEN, Piece.Color.WHITE));
-        verifyKingType(Piece.create(Piece.Type.KING, Piece.Color.WHITE));
+        verifyPawnType(Piece.createWhitePawn());
+        verifyKnightType(Piece.createWhiteKnight());
+        verifyRookType(Piece.createWhiteRook());
+        verifyBishopType(Piece.createWhiteBishop());
+        verifyQueenType(Piece.createWhiteQueen());
+        verifyKingType(Piece.createWhiteKing());
+        verifyNoPieceType(Piece.createNoPiece());
     }
 
     private void verifyPawnType(Piece piece) {
@@ -55,7 +56,14 @@ class PieceTest {
     private void verifyQueenType(Piece piece) {
         assertEquals(true, piece.isQueen());
     }
+
     private void verifyKingType(Piece piece) {
         assertEquals(true, piece.isKing());
     }
+
+    private void verifyNoPieceType(Piece piece) {
+        assertEquals(true, piece.isNoPiece());
+    }
+
+
 }

5817d26722278a1b470db67522a8cb045ffe4f55
이병찬 <lbc@ibyeongchan-ui-noteubug.local>
2023-07-10 15:55:33 +0900

    feat: 각 TYPE에 점수 추가

--- src/main/java/softeer2nd/chess/Constants.java
+++ src/main/java/softeer2nd/chess/Constants.java
@@ -8,19 +8,29 @@ public class Constants {
 
     public enum Type {
 
-        PAWN('p'), KNIGHT('n'), ROOK('r'),
-        BISHOP('b'), QUEEN('q'), KING('k'),
-        NO_PIECE('.');
+        PAWN('p', 1.0),
+        KNIGHT('n', 2.5),
+        ROOK('r', 5.0),
+        BISHOP('b', 3.0),
+        QUEEN('q', 9.0),
+        KING('k', 0.0),
+        NO_PIECE('.', 0.0);
 
         private final char representation;
+        private final double defaultPoint;
 
-        Type(char representation) {
+        Type(char representation, double defaultPoint) {
 
             this.representation = representation;
+            this.defaultPoint = defaultPoint;
         }
 
         public char getRepresentation() {
             return this.representation;
+        }
+
+        public double getDefaultPoint() {
+            return this.defaultPoint;
         }
     }
 }

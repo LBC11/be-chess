8d470c9f319dbedca190d053e33fb2f1a49c7287
이병찬 <lbc@ibyeongchan-ui-noteubug.local>
2023-07-11 10:21:15 +0900

    feat: PIECECOMPARATOR 구현

--- src/main/java/softeer2nd/chess/pieces/Piece.java
+++ src/main/java/softeer2nd/chess/pieces/Piece.java
@@ -2,8 +2,10 @@ package softeer2nd.chess.pieces;
 
 import softeer2nd.chess.Constants.Type;
 import softeer2nd.chess.Constants.Color;
+import softeer2nd.chess.Constants.SortOrder;
 import softeer2nd.utils.StringUtils;
 
+import java.util.Comparator;
 import java.util.Objects;
 
 public class Piece {
@@ -40,8 +42,25 @@ public class Piece {
         return this.color.equals(color);
     }
 
-    public boolean isPawn(final Piece piece) {
+    public boolean isPawn() {
         return this.type.equals(Type.PAWN);
+    }
+
+    public static class PieceComparator implements Comparator<Piece> {
+
+        private final SortOrder sortOrder;
+
+        public PieceComparator(SortOrder sortOrder) {
+            this.sortOrder = sortOrder;
+        }
+
+        @Override
+        public int compare(Piece o1, Piece o2) {
+
+            int result = (int) (o1.getType().getDefaultPoint() - o2.getType().getDefaultPoint());
+
+            return sortOrder == SortOrder.ASCENDING ? result : -result;
+        }
     }
 
     @Override

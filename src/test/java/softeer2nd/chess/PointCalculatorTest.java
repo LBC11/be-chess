047722eee690de235084de7a8932604f54f63f75
이병찬 <lbc@ibyeongchan-ui-noteubug.local>
2023-07-12 19:54:47 +0900

    test: PointCalculator의 테스트 추가

---
+++ src/test/java/softeer2nd/chess/PointCalculatorTest.java
@@ -0,0 +1,34 @@
+package softeer2nd.chess;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PointCalculatorTest {
+
+    PointCalculator pointCalculator;
+    Board board;
+
+
+    @BeforeEach
+    void setup() {
+        board = new Board();
+        board.initialize();
+        pointCalculator = new PointCalculator(board);
+    }
+
+    @Test
+    @DisplayName("하얀색 기물들의 점수가 정상적으로 반환되어야 한다.")
+    void calculateWhitePoint() {
+        assertEquals(38.0, pointCalculator.calculateWhitePoint());
+    }
+
+    @Test
+    @DisplayName("검은색 기물들의 점수가 정상적으로 반환되어야 한다.")
+    void calculateBlackPoint() {
+        assertEquals(38.0, pointCalculator.calculateBlackPoint());
+    }
+
+}

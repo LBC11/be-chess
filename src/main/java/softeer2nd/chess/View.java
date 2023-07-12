8e03821b2a3302c8e002433b9d511fa8015baa3f
이병찬 <lbc@ibyeongchan-ui-noteubug.local>
2023-07-12 19:46:07 +0900

    refactor: 사용하지 않는 상수 제거

--- src/main/java/softeer2nd/chess/View.java
+++ src/main/java/softeer2nd/chess/View.java
@@ -1,14 +1,6 @@
 package softeer2nd.chess;
 
-import softeer2nd.utils.StringUtils;
-
-import java.util.stream.IntStream;
-
 public class View {
-
-    private final int ROW_LENGTH = Constants.BoardConfig.ROW_LENGTH.getValue();
-    private final int COLUMN_LENGTH = Constants.BoardConfig.COLUMN_LENGTH.getValue();
-    private final int KEY_GENERATION_MULTIPLIER = Constants.BoardConfig.KEY_GENERATION_MULTIPLIER.getValue();
 
     private final Board board;
 

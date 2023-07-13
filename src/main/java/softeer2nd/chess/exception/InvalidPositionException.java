b789c21063b73096e9a3d5fbef57ac9eeac2033e
이병찬 <lbc@ibyeongchan-ui-noteubug.local>
2023-07-13 21:17:43 +0900

    feat: InvalidPositionException 구현

---
+++ src/main/java/softeer2nd/chess/exception/InvalidPositionException.java
@@ -0,0 +1,7 @@
+package softeer2nd.chess.exception;
+
+public class InvalidPositionException extends RuntimeException {
+    public InvalidPositionException(String message) {
+        super(message);
+    }
+}

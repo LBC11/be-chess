d9c8b2fcba2951b45a0ff01619ff1604ab96fadc
이병찬 <lbc@ibyeongchan-ui-noteubug.local>
2023-07-13 21:22:53 +0900

    feat: Position 예외 상황 적용

--- src/main/java/softeer2nd/chess/Board/Position.java
+++ src/main/java/softeer2nd/chess/Board/Position.java
@@ -1,5 +1,7 @@
 package softeer2nd.chess.Board;
 
+import softeer2nd.chess.exception.InvalidPositionException;
+
 import static softeer2nd.chess.Board.Constants.COLUMN_LENGTH;
 
 public class Position {
@@ -8,9 +10,12 @@ public class Position {
     private final int yPos;
 
     public Position(String position) {
-
         this.xPos = position.charAt(0) - 'a';
         this.yPos = COLUMN_LENGTH - (position.charAt(1) - '0');
+
+        if (!isValidPosition(position) || !isWithinBoardBounds(xPos, yPos)) {
+            throw new InvalidPositionException("Invalid position: " + position);
+        }
     }
 
     public int getYPos() {
@@ -19,6 +24,14 @@ public class Position {
 
     public int getXPos() {
         return xPos;
+    }
+
+    private boolean isWithinBoardBounds(int x, int y) {
+        return x >= 0 && x < 8 && y >= 0 && y < 8;
+    }
+
+    private boolean isValidPosition(String position) {
+        return position.length() == 2;
     }
 
 }

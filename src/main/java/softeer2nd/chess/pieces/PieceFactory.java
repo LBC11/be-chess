307120b51c1ed0eb58caafa40f22cbf5a6a4c27e
이병찬 <lbc@ibyeongchan-ui-noteubug.local>
2023-07-13 19:25:17 +0900

    feat: PieceFactory 구현
    - 각 type, class 에 맞게 기물 생성
    - 필드 데이터가 이상하면 InvalidPieceFieldException 발생

---
+++ src/main/java/softeer2nd/chess/pieces/PieceFactory.java
@@ -0,0 +1,55 @@
+package softeer2nd.chess.pieces;
+
+import softeer2nd.chess.exception.InvalidColorException;
+import softeer2nd.chess.exception.InvalidPieceFieldException;
+import softeer2nd.chess.exception.InvalidTypeException;
+
+import static softeer2nd.chess.Board.Constants.*;
+import static softeer2nd.chess.Board.Constants.Color.*;
+
+public enum PieceFactory {
+
+    INSTANCE;
+
+    public static Piece create(Type type, Color color) {
+
+        verifyField(type, color);
+
+        switch (type) {
+            case PAWN -> {
+                if (color == WHITE) {
+                    return new WhitePawn();
+                }
+
+                else {
+                    return new BlackPawn();
+                }
+            }
+            case KNIGHT -> {
+                return new Knight(color);
+            }
+            case ROOK -> {
+                return new Rook(color);
+            }
+            case BISHOP -> {
+                return new Bishop(color);
+            }
+            case QUEEN -> {
+                return new Queen(color);
+            }
+            case KING -> {
+                return new King(color);
+            }
+            case NO_PIECE -> {
+                return new Blank(color);
+            }
+
+            default -> throw new InvalidTypeException();
+        }
+    }
+
+    private static void verifyField(Type type, Color color) throws InvalidPieceFieldException{
+        if(type == Type.NO_PIECE && color != NOCOLOR) throw new InvalidTypeException();
+        if(type != Type.NO_PIECE && color == NOCOLOR) throw new InvalidColorException();
+    }
+}

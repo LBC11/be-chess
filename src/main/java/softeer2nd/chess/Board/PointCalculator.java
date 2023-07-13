dc0f31993c03c5b275116522ef3a52fb09b077c4
이병찬 <lbc@ibyeongchan-ui-noteubug.local>
2023-07-13 21:07:26 +0900

    refactor: Constants, PointCalculator 에서 final 되어 있지 않은 field 수정

--- src/main/java/softeer2nd/chess/Board/PointCalculator.java
+++ src/main/java/softeer2nd/chess/Board/PointCalculator.java
@@ -1,12 +1,11 @@
 package softeer2nd.chess.Board;
 
-import softeer2nd.chess.Board.Board;
 
 import static softeer2nd.chess.Board.Constants.*;
 
 public class PointCalculator {
 
-    private Board board;
+    private final Board board;
 
     public PointCalculator(Board board) {
         this.board = board;

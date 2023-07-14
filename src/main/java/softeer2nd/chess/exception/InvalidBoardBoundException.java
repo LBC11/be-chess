9b5e303e7816d5cbc96ec04f8638ae8451a3d6d8
이병찬 <lbc@ibyeongchan-ui-noteubug.local>
2023-07-14 11:40:44 +0900

    feat: Position에서 보드판을 벗어나는 position을 위한 예외 구현

---
+++ src/main/java/softeer2nd/chess/exception/InvalidBoardBoundException.java
@@ -0,0 +1,7 @@
+package softeer2nd.chess.exception;
+
+public class InvalidBoardBoundException extends RuntimeException{
+    public InvalidBoardBoundException(String message) {
+        super(message);
+    }
+}

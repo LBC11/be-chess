10d6854093838087dc10690ba563c7f0eec9399a
이병찬 <lbc@ibyeongchan-ui-noteubug.local>
2023-07-14 13:51:59 +0900

    feat: 기물을 움직일 떄 생기는예외(대표 예외, 거리 부족) 구현

---
+++ src/main/java/softeer2nd/chess/exception/InvalidDistanceException.java
@@ -0,0 +1,7 @@
+package softeer2nd.chess.exception;
+
+public class InvalidDistanceException extends InvalidMoveException {
+    public InvalidDistanceException(String message) {
+        super(message);
+    }
+}

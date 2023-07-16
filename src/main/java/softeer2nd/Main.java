092e63d0ca286c7c7a25ca2f85d3f42be93bfcdc
이병찬 <lbc@ibyeongchan-ui-noteubug.local>
2023-07-16 19:21:43 +0900

    refactor: 객체지향을 위해 Chess의 로직, 명령어 입출력을 Chess 클래스 내부로 캡슐화
    - 명령어 상수로 선언
    - public이던 메소드 private로 수정
    - private 메소드는 테스트 하지 않으므로 ChessTest삭제

--- src/main/java/softeer2nd/Main.java
+++ src/main/java/softeer2nd/Main.java
@@ -2,36 +2,13 @@ package softeer2nd;
 
 import softeer2nd.chess.Chess;
 
-import java.util.Scanner;
-
 public class Main {
 
     static Chess chess;
 
     public static void main(String[] args) {
-
-        Scanner sc = new Scanner(System.in);
-
         chess = new Chess();
-        chess.init();
+        chess.start();
 
-        // 2중 반복문을 한번에 종료하기 위한 라벨
-        outer:
-        while(true) {
-
-            System.out.print("명령어를 입력해주세요: ");
-            String[] cmd = sc.nextLine().split(" ");
-
-            switch (cmd[0]) {
-                case "start" -> System.out.println(chess.start());
-                case "end" -> System.out.println(chess.end());
-                case "move" -> chess.move(cmd[1], cmd[2]);
-                case "exit" -> {
-                    System.out.println("시스템을 종료합니다.");
-                    break outer;
-                }
-                default -> System.out.println("올바른 명령어가 아닙니다.");
-            }
-        }
     }
 }
--- src/main/java/softeer2nd/chess/Chess.java
+++ src/main/java/softeer2nd/chess/Chess.java
@@ -3,15 +3,22 @@ package softeer2nd.chess;
 import softeer2nd.chess.Board.Board;
 import softeer2nd.chess.Board.PointCalculator;
 import softeer2nd.chess.Board.chessView;
+import softeer2nd.chess.exception.moveException.InvalidSamePositionException;
+
+import java.util.Scanner;
 
 public class Chess {
+
+    private static final String START = "start";
+    private static final String END = "end";
+    private static final String MOVE = "move";
 
     private Board board;
     private chessView chessView;
     private PointCalculator pointCalculator;
-    private boolean isStarted;
+    private boolean isContinue;
 
-    public void init() {
+    private void init() {
         board = new Board();
         board.initialize();
 
@@ -19,32 +26,69 @@ public class Chess {
 
         chessView = new chessView(board, pointCalculator);
 
-        isStarted = false;
+        isContinue = false;
     }
 
-    public String start() {
+    public void start() {
 
-        if(isStarted) return "이미 게임중입니다.";
-        
+        Scanner sc = new Scanner(System.in);
+
+        // 2중 반복문을 한번에 종료하기 위한 라벨
+        while(isContinue) {
+
+            System.out.print("명령어를 입력해주세요: ");
+            String[] cmd = sc.nextLine().split(" ");
+
+            switch (cmd[0]) {
+                case START -> gameStart();
+                case END -> end();
+                case MOVE -> move(cmd[1], cmd[2]);
+                default -> InvalidCommand();
+            }
+        }
+    }
+
+    private void InvalidCommand() {
+        System.out.println("올바른 명령어가 아닙니다. start/ end/ move b1 b2 명령어를 이용해주세요. ");
+    }
+
+    private void gameStart() {
+        init();
+
+        if(isContinue) {
+            System.out.println("이미 게임중입니다.");
+            return;
+        }
+
+        System.out.println("게임이 시작되었습니다.");
+
         chessView.showBoard();
         chessView.showPoint();
-        isStarted = true;
-        return "게임이 시작되었습니다.";
+        isContinue = true;
     }
 
-    public String end() {
+    private void end() {
 
-        if(!isStarted) return "게임를 하고 있지 않습니다.";
+        if(!isContinue) {
+            System.out.println("게임를 하고 있지 않습니다.");
+            return;
+        }
 
-        isStarted = false;
-        return "게임이 종료되었습니다.";
+        isContinue = false;
+        System.out.println("게임이 종료되었습니다.");
     }
 
-    public void move(String sourcePosition, String targetPosition) {
-        if(!isStarted) System.out.println("게임을 하고 있지 않습니다.");
+    private void move(String sourcePosition, String targetPosition) {
+        if(!isContinue) System.out.println("게임을 하고 있지 않습니다.");
+
+        if(verifySameLocation(sourcePosition, targetPosition)) throw new InvalidSamePositionException(sourcePosition, targetPosition);
 
         board.move(sourcePosition, targetPosition);
         chessView.showBoard();
         chessView.showPoint();
+    }
+
+    private boolean verifySameLocation(final String sourcePosition, final String targetPosition) {
+        return sourcePosition.equals(targetPosition);
     }
 }

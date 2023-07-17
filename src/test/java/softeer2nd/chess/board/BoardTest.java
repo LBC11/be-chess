d1665ccfa1fa8961d10fea15032fce0b226734ca
이병찬 <lbc@ibyeongchan-ui-noteubug.local>
2023-07-17 17:34:13 +0900

    fix: 턴제를 적용했을 떄를 위해 테스트 수정

--- src/test/java/softeer2nd/chess/board/BoardTest.java
+++ src/test/java/softeer2nd/chess/board/BoardTest.java
@@ -3,17 +3,17 @@ package softeer2nd.chess.board;
 
 import softeer2nd.chess.Board.Board;
 import softeer2nd.chess.Board.Constants.Type;
-import softeer2nd.chess.Board.Constants.Color;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static softeer2nd.chess.Board.Constants.Color.*;
 import static softeer2nd.utils.StringUtils.appendNewLine;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import softeer2nd.chess.exception.moveException.InvalidBlockedMoveException;
-import softeer2nd.chess.exception.moveException.InvalidColorMoveException;
+import softeer2nd.chess.exception.moveException.InvalidSameColorException;
 import softeer2nd.chess.exception.moveException.UnreachablePositionException;
 import softeer2nd.chess.pieces.PieceFactory;
 
@@ -48,28 +48,28 @@ class BoardTest {
     @DisplayName("board 의 piece 개수가 정상적으로 반환된다.")
     void pieceTypeColorCount() {
 
-        assertEquals(8, board.pieceCount(Type.PAWN, Color.BLACK));
-        assertEquals(8, board.pieceCount(Type.PAWN, Color.WHITE));
-        assertEquals(2, board.pieceCount(Type.KNIGHT, Color.BLACK));
-        assertEquals(2, board.pieceCount(Type.KNIGHT, Color.WHITE));
-        assertEquals(2, board.pieceCount(Type.ROOK, Color.BLACK));
-        assertEquals(2, board.pieceCount(Type.ROOK, Color.WHITE));
-        assertEquals(2, board.pieceCount(Type.BISHOP, Color.BLACK));
-        assertEquals(2, board.pieceCount(Type.BISHOP, Color.WHITE));
-        assertEquals(1, board.pieceCount(Type.QUEEN, Color.BLACK));
-        assertEquals(1, board.pieceCount(Type.QUEEN, Color.WHITE));
-        assertEquals(1, board.pieceCount(Type.KING, Color.BLACK));
-        assertEquals(1, board.pieceCount(Type.KING, Color.WHITE));
+        assertEquals(8, board.pieceCount(Type.PAWN, BLACK));
+        assertEquals(8, board.pieceCount(Type.PAWN, WHITE));
+        assertEquals(2, board.pieceCount(Type.KNIGHT, BLACK));
+        assertEquals(2, board.pieceCount(Type.KNIGHT, WHITE));
+        assertEquals(2, board.pieceCount(Type.ROOK, BLACK));
+        assertEquals(2, board.pieceCount(Type.ROOK, WHITE));
+        assertEquals(2, board.pieceCount(Type.BISHOP, BLACK));
+        assertEquals(2, board.pieceCount(Type.BISHOP, WHITE));
+        assertEquals(1, board.pieceCount(Type.QUEEN, BLACK));
+        assertEquals(1, board.pieceCount(Type.QUEEN, WHITE));
+        assertEquals(1, board.pieceCount(Type.KING, BLACK));
+        assertEquals(1, board.pieceCount(Type.KING, WHITE));
     }
 
     @Test
     @DisplayName("위치에 따른 기물이 정상적으로 반환된다.")
     void findPiece() {
 
-        assertEquals(PieceFactory.create(Type.ROOK, Color.BLACK), board.findPiece("a8"));
-        assertEquals(PieceFactory.create(Type.ROOK, Color.BLACK), board.findPiece("h8"));
-        assertEquals(PieceFactory.create(Type.ROOK, Color.WHITE), board.findPiece("a1"));
-        assertEquals(PieceFactory.create(Type.ROOK, Color.WHITE), board.findPiece("h1"));
+        assertEquals(PieceFactory.create(Type.ROOK, BLACK), board.findPiece("a8"));
+        assertEquals(PieceFactory.create(Type.ROOK, BLACK), board.findPiece("h8"));
+        assertEquals(PieceFactory.create(Type.ROOK, WHITE), board.findPiece("a1"));
+        assertEquals(PieceFactory.create(Type.ROOK, WHITE), board.findPiece("h1"));
     }
 
     @Test
@@ -77,13 +77,13 @@ class BoardTest {
     void move() {
         board.initialize();
 
-        String sourcePosition = "b2";
-        String targetPosition = "b3";
+        String sourcePosition = "b7";
+        String targetPosition = "b6";
         
-        board.move(sourcePosition, targetPosition);
+        board.move(sourcePosition, targetPosition, BLACK);
 
-        assertEquals(PieceFactory.create(Type.NO_PIECE, Color.NOCOLOR), board.findPiece(sourcePosition));
-        assertEquals(PieceFactory.create(Type.PAWN, Color.WHITE), board.findPiece(targetPosition));
+        assertEquals(PieceFactory.create(Type.NO_PIECE, NOCOLOR), board.findPiece(sourcePosition));
+        assertEquals(PieceFactory.create(Type.PAWN, BLACK), board.findPiece(targetPosition));
     }
 
     @Test
@@ -91,11 +91,11 @@ class BoardTest {
     void move_UnreachablePositionException() {
         board.initialize();
 
-        String sourcePosition = "b2";
-        String targetPosition = "b4";
+        String sourcePosition = "a2";
+        String targetPosition = "a4";
 
         assertThrows(UnreachablePositionException.class, () ->
-                board.move(sourcePosition, targetPosition));
+                board.move(sourcePosition, targetPosition, WHITE));
     }
 
     @Test
@@ -103,23 +103,23 @@ class BoardTest {
     void move_InvalidBlockedMoveException() {
         board.initialize();
 
-        String sourcePosition = "d1";
-        String targetPosition = "d3";
+        String sourcePosition = "d8";
+        String targetPosition = "d5";
 
         assertThrows(InvalidBlockedMoveException.class, () ->
-                board.move(sourcePosition, targetPosition));
+                board.move(sourcePosition, targetPosition, BLACK));
     }
 
     @Test
-    @DisplayName("시작점과 도착점의 색깔이 같을 떄 InvalidColorMoveException 이 실행되어야 한다.")
+    @DisplayName("시작점과 도착점의 색깔이 같을 떄 InvalidSameColorException 이 실행되어야 한다.")
     void move_InvalidColorMoveException() {
         board.initialize();
 
         String sourcePosition = "d1";
         String targetPosition = "d2";
 
-        assertThrows(InvalidColorMoveException.class, () ->
-                board.move(sourcePosition, targetPosition));
+        assertThrows(InvalidSameColorException.class, () ->
+                board.move(sourcePosition, targetPosition, WHITE));
     }
 
     @Test
@@ -127,23 +127,23 @@ class BoardTest {
     void calculatePoint() {
         board.initializeEmpty();
 
-        board.addPiece("b6", PieceFactory.create(Type.PAWN, Color.BLACK));
-        board.addPiece("e6", PieceFactory.create(Type.QUEEN, Color.BLACK));
-        board.addPiece("b8", PieceFactory.create(Type.KING, Color.BLACK));
-        board.addPiece("c8", PieceFactory.create(Type.ROOK, Color.BLACK));
+        board.addPiece("b6", PieceFactory.create(Type.PAWN, BLACK));
+        board.addPiece("e6", PieceFactory.create(Type.QUEEN, BLACK));
+        board.addPiece("b8", PieceFactory.create(Type.KING, BLACK));
+        board.addPiece("c8", PieceFactory.create(Type.ROOK, BLACK));
 
-        board.addPiece("f2", PieceFactory.create(Type.PAWN, Color.WHITE));
-        board.addPiece("g2", PieceFactory.create(Type.PAWN, Color.WHITE));
-        board.addPiece("e1", PieceFactory.create(Type.ROOK, Color.WHITE));
-        board.addPiece("f1", PieceFactory.create(Type.KING, Color.WHITE));
+        board.addPiece("f2", PieceFactory.create(Type.PAWN, WHITE));
+        board.addPiece("g2", PieceFactory.create(Type.PAWN, WHITE));
+        board.addPiece("e1", PieceFactory.create(Type.ROOK, WHITE));
+        board.addPiece("f1", PieceFactory.create(Type.KING, WHITE));
 
         // 0.01은 오차범위를 의미한다.
-        assertEquals(15.0, board.calculatePoint(Color.BLACK), 0.01);
-        assertEquals(7.0, board.calculatePoint(Color.WHITE), 0.01);
+        assertEquals(15.0, board.calculatePoint(BLACK), 0.01);
+        assertEquals(7.0, board.calculatePoint(WHITE), 0.01);
 
         // Pawn 이 같은 세로줄에 2개 이상 있는 경우
-        board.addPiece("b5", PieceFactory.create(Type.PAWN, Color.BLACK));
-        assertEquals(15.0, board.calculatePoint(Color.BLACK), 0.01);
+        board.addPiece("b5", PieceFactory.create(Type.PAWN, BLACK));
+        assertEquals(15.0, board.calculatePoint(BLACK), 0.01);
 
     }
 }
